28a8253c973aee51c2bc4fbf85701a80dbfabad8
Jawed Ashraf <Jawed@cupidity.f9.co.uk>
2013-08-28 19:43:00 +0100

    Removed more irrelevant stuff

--- resource.h
+++ resource.h
@@ -14,13 +14,3 @@
 #define RC_NLM_SINGLE	10003
 #define RC_NLM_MULTI	10004
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
